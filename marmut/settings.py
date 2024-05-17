a9cedf9626653c738c1274d582561a309ae08cb6
Rizki Maulana <rizki.maulana23@ui.ac.id>
2024-05-17 21:24:33 +0700

    gas

--- marmut/settings.py
+++ marmut/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-676b&xfm03-ck_%y!!rr#kw=^q(ch73ws#m@*4w^ovknzd!12j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
